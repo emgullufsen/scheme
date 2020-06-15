b55a938901a04ddd52ec0b2ba8b0f818d58b5e8e
rickygee <ericgullufsen@gmail.com>
2020-06-14 19:03:31 -0800

    started next ex

--- ch3.rkt
+++ ch3.rkt
@@ -1,4 +1,6 @@
 #lang scheme
+(define (gcd a b)
+    (if (= b 0) a (gcd b (remainder a b))))
 (define (make-accumulator initial-amount)
     (lambda (n) 
         (begin (set! initial-amount (+ initial-amount n)) initial-amount)))
@@ -33,3 +35,24 @@
     dispatch))
 
 (define (call-the-cops) (display "you did it now bud!"))
+
+(define rand
+  (let ((x random-init))
+    (lambda () (set! x (rand-update x)) x)))
+
+(define (estimate-pi trials)
+  (sqrt (/ 6 (monte-carlo trials cesaro-test))))
+(define (cesaro-test)
+   (= (gcd (rand) (rand)) 1))
+
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining 0)
+           (/ trials-passed trials))
+          ((experiment)
+           (iter (- trials-remaining 1) 
+                 (+ trials-passed 1)))
+          (else
+           (iter (- trials-remaining 1) 
+                 trials-passed))))
+  (iter trials 0))
