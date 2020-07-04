0d174f04555cef552ee623413b8fec9bb0269846
ricky gee <ericgullufsen@gmail.com>
2020-07-03 18:12:08 -0800

    sandbox file for chapter three

---
+++ ch3-sandbo.rkt
@@ -0,0 +1,15 @@
+#lang scheme
+(define (make-withdraw initial-amount)
+  (let ((balance initial-amount))
+    (lambda (amount)
+      (if (>= balance amount)
+          (begin (set! balance (- balance amount)) balance)
+          "Insufficient funds"))))
+
+(define (make-withdraw2 initial-amount)
+    ((lambda (balance) 
+        (lambda (amount)
+            (if (>= balance amount)
+                (begin (set! balance (- balance amount)) balance)
+                "Insufficient funds"))
+      ) initial-amount))
