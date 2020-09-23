364c34d88a9457dac1716b9a0b1f958c19256d1b
rickygee <ericgullufsen@gmail.com>
2020-09-22 20:59:50 -0800

    exercises 3.28, 3.29 done

--- ChapterTree/dc.scm
+++ ChapterTree/dc.scm
@@ -16,3 +16,85 @@
              ((eq? m 'add-action!) accept-action-procedure!)
              (else (error "Unknown operation -- WIRE" m))))
      dispatch))
+
+(define (half-adder a b s c)
+    (let ((d (make-wire)) (e (make-wire)))
+          (or-gate a b d)
+	  (and-gate a b c)
+	  (inverter c e)
+          (and-gate d e s)
+          'ok))
+
+(define (full-adder a b c-in sum c-out)
+    (let ((c1 (make-wire)) 
+          (c2 (make-wire))
+	  (s  (make-wire)))
+       	  	(half-adder b c-in s c1)
+        	(half-adder a s sum c2)
+          	(or-gate c1 c2 c-out)
+          	'ok))
+
+(define (inverter input output)
+    (define (invert-input)
+          (let ((new-value (logical-not (get-signal input))))
+	          (after-delay inverter-delay
+		          (lambda ()
+		             (set-signal! output new-value)))))
+    (add-action! input invert-input)
+    'ok)
+
+(define (logical-not s)
+    (cond ((= s 0) 1)
+          ((= s 1) 0)
+          (else (error "Invalid signal" s))))
+
+(define (logical-and s1 s2)
+  (cond ((and (= s1 1) (= s2 1)) 1)
+	((or (= s1 0) (= s2 0)) 0)
+	(else (error "Invalid signal (log-and)"))))
+
+(define (and-gate a1 a2 output)
+    (define (and-action-procedure)
+          (let ((new-value (logical-and (get-signal a1) (get-signal a2))))
+          	(after-delay and-gate-delay
+			(lambda ()
+		             (set-signal! output new-value)))))
+    (add-action! a1 and-action-procedure)
+    (add-action! a2 and-action-procedure)
+    'ok)
+
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure)
+    (let ((new-value (logical-or (get-signal a1) (get-signal a2))))
+      (after-delay or-gate-delay
+		   (lambda () (set-signal! output new-value)))))
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
+  'ok)
+
+(define (after-delay d f) (display "nothing for now (after-delay)..."))
+
+(define (or-gate2 a1 a2 output)
+  (let ((i1 (make-wire)) (i2 (make-wire)) (outinv (make-wire)))
+    (inverter a1 i1)
+    (inverter a2 i2)
+    (and-gate i1 i2 outinv)
+    (define (or-gate2-procedure)
+      (let ((new-value (logical-not (get-signal outinv))))
+	(after-delay or-gate-delay
+		     (lambda () (set-signal! output new-value)))))
+    (add-action! a1 or-gate2-procedure)
+    (add-action! a2 or-gate2-procedure)
+    'ok))
+
+(define (call-each procedures)
+    (if (null? procedures)
+    	'done
+        (begin ((car procedures)) (call-each (cdr procedures)))))
+
+(define (get-signal wire)
+    (wire 'get-signal))
+(define (set-signal! wire new-value)
+    ((wire 'set-signal!) new-value))
+(define (add-action! wire action-procedure)
+    ((wire 'add-action!) action-procedure))
