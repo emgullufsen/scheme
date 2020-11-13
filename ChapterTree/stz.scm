58aa7a1813b2592eb90880bc40d4b60ee9d57f26
rickygee <ericgullufsen@gmail.com>
2020-11-12 19:34:15 -0900

    exercise 3.54 done

--- ChapterTree/stz.scm
+++ ChapterTree/stz.scm
@@ -6,6 +6,16 @@
   (display-line x)
   x)
 
+(define (add-streams s1 s2) (stream-map + s1 s2))
+
+(define (mul-streams s1 s2) (stream-map * s1 s2))
+
+(define ones (cons-stream 1 ones))
+
+(define integers (cons-stream 1 (add-streams ones integers)))
+ 
+(define factorials (cons-stream 1 (mul-streams factorials integers)))
+
 (define (stream-enumerate-interval low high)
   (if (> low high)
       the-empty-stream
