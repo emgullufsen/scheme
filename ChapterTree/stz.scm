59857aea5ab59ca62714dd3c55b7f08930a8e2e9
rik gee <ericgullufsen@gmail.com>
2020-11-28 17:01:41 -0900

    ex 3.56 done

--- ChapterTree/stz.scm
+++ ChapterTree/stz.scm
@@ -44,3 +44,32 @@
   (stream-map 
    show 
    (stream-enumerate-interval 0 10)))
+
+(define (merge s1 s2)
+  (cond ((stream-null? s1) s2)
+        ((stream-null? s2) s1)
+        (else
+         (let ((s1car (stream-car s1))
+               (s2car (stream-car s2)))
+           (cond ((< s1car s2car)
+                  (cons-stream 
+                   s1car 
+                   (merge (stream-cdr s1) 
+                          s2)))
+                 ((> s1car s2car)
+                  (cons-stream 
+                   s2car 
+                   (merge s1 
+                          (stream-cdr s2))))
+                 (else
+                  (cons-stream 
+                   s1car
+                   (merge 
+                    (stream-cdr s1)
+                    (stream-cdr s2)))))))))
+
+(define (scale-stream s c)
+  (stream-map (lambda (x) (* c x)) s))
+
+;; exercise 3.56
+(define S1 (cons-stream 1 (merge (scale-stream S1 2) (merge (scale-stream S1 3) (scale-stream S1 5)))))
