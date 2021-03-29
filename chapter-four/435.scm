5471e9f1af337cfa2b5d6cacaaf8c4c55ca4b2a2
rickyman <ericgullufsen@gmail.com>
2021-03-29 14:03:26 -0800

    into the logic puzzles stuff now

--- chapter-four/435.scm
+++ chapter-four/435.scm
@@ -3,6 +3,17 @@
 (define (require p)
   (if (not p) (amb)))
 
+(define (distinct? l)
+  (if (null? l)
+      true
+      (let ((f (car l)) (s (cdr l)))
+	    (if (null? s)
+		true
+		(let ((fs (car s)))
+		      (if (= f fs)
+			  false
+			  (distinct? s)))))))
+
 (define (an-integer-between l h)
  (require (<= l h))
  (amb l (an-integer-between (+ l 1) h)))
